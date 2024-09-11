bc6fe23573a8287a3ae05cf031efe54fae045f54
BurnsCaleb <161860534+BurnsCaleb@users.noreply.github.com>
2024-09-11 11:49:44 -0500

    Add files via upload

---
+++ ScorecardDriver.java
@@ -0,0 +1,14 @@
+package Driver;
+
+/**
+ * Caleb Burns - cdburns1
+ * CIS171 21708-25949
+ * Apr 29, 2024
+ */
+public class ScorecardDriver {
+	public static void main(String[] args) {
+		// Start GUI
+		view.ScorecardStarter.main(args);
+		
+	}
+}
